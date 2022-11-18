00da91e30bfd174c1522d349597c1fd861aebc0f
yashmitawa122333 <mitawayash7@gmail.com>
2022-11-18 15:08:50 +0530

    Add the serve state and done state

--- pong/pong-3/main.lua
+++ pong/pong-3/main.lua
@@ -23,13 +23,14 @@ function love.load()
         resizable = false,
         vsync = true
     })
+    servingPlayer = 1
+
     -- initilize the variable for the score
     player1Score = 0
     player2Score = 0
 
     player1 = Paddle(10, 30, 5, 20)
     player2 = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30, 5, 20)
-
     ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
     -- initilize the state of the game 
     gameState = 'start'
@@ -38,7 +39,15 @@ end
 
 function love.update(dt)
     -- Adding the velocity and the movements to the ball
-    if gameState == 'play' then
+    if gameState == 'serve' then
+        ball.dy = math.random(-50, 50)
+        if servingPlayer == 1 then
+            ball.dx = math.random(140, 200)
+        else
+            ball.dx = -math.random(140, 200)
+        end
+
+    elseif gameState == 'play' then
         if ball:collides(player1) then
             ball.dx = -ball.dx * 1.03
             ball.x = player1.x + 5
@@ -60,23 +69,41 @@ function love.update(dt)
             end
         end
         
-        if ball.y < 0 then
+        if ball.y <= 0 then
             ball.y = 0
             ball.dy = -ball.dy
         end
-        if ball.y > VIRTUAL_HEIGHT - 4 then
+        if ball.y >= VIRTUAL_HEIGHT - 4 then
             ball.y = VIRTUAL_HEIGHT - 4
             ball.dy = -ball.dy
         end
     end
 
+    -- Score Update When Colision 
     if ball.x < 0 then
+        servingPlayer = 1
         player1Score = player1Score + 1
-        ball:reset()
+        
+        if player2Score == 10 then 
+            winningPlayer = 2
+            gameState = 'done'
+        else
+            gameState = 'serve'
+            ball:reset()
+        end
     end
+
     if ball.x > VIRTUAL_WIDTH then
+        servingPlayer = 2
         player2Score = player2Score + 1
-        ball:reset()
+
+        if player1Score == 10 then
+            winningPlayer = 1
+            gameState = 'done'
+        else
+            gameState = 'serve'
+            ball:reset()
+        end
     end
 
     -- player1 movements 
@@ -110,11 +137,11 @@ function love.keypressed(key)
         love.event.quit()
     elseif key == 'enter' or key == 'return' then
         if gameState == 'start' then
+            gameState = 'serve'
+        elseif gameState == 'serve' then
             gameState = 'play'
-        else
+        elseif gameState == 'done' then
             gameState = 'start'
-            -- initilize the ball movements again at the start
-            ball:reset()
         end
     end
 end
@@ -124,15 +151,24 @@ function love.draw()
 
     love.graphics.clear(0, 0, 0, 255)
 
+    -- Display the score of both player
+    displayScore()
+
     if gameState == 'start' then
-        love.graphics.printf('Hello Pong !', 0, 20, VIRTUAL_WIDTH, 'center')
-    else
-        love.graphics.printf('Play state !', 0, 20, VIRTUAL_WIDTH, 'center')
+        love.graphics.printf('Welcome to Pong!', 0, 10, VIRTUAL_WIDTH, 'center')
+        love.graphics.printf('Press Enter to begin!', 0, 20, VIRTUAL_WIDTH, 'center')
+    elseif gameState == 'serve' then
+        love.graphics.printf('Player ' .. tostring(servingPlayer) .. "'s serve!", 
+            0, 10, VIRTUAL_WIDTH, 'center')
+        love.graphics.printf('Press Enter to serve!', 0, 20, VIRTUAL_WIDTH, 'center')
+    elseif gameState == 'done' then 
+        love.graphics.printf('Player '.. tostring(winningPlayer) .." hurry !",
+            0, 10, VIRTUAL_WIDTH, 'center')
+    elseif gameState == 'play' then
+        -- no UI messages to display in play
     end
 
-    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH/2 - 50, VIRTUAL_HEIGHT / 3)
-    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH/2 + 40, VIRTUAL_HEIGHT / 3)
-
+    
     -- draw the ball and the player1 and player2Y
     player1:render()
     player2:render()
@@ -147,4 +183,9 @@ end
 function displayFPS()
     love.graphics.setColor(0, 255, 0, 255)
     love.graphics.print('FPS..'..tostring(love.timer.getFPS()), 10, 10)
+end
+
+function displayScore()
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH/2 - 50, VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH/2 + 40, VIRTUAL_HEIGHT / 3)
 end
